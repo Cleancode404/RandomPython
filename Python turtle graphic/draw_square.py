2bd16cfb699b829be723f7ea94584b68091f1e31
Heuristic <codemonkey2468@hotmail.com>
2020-10-21 09:34:02 -0400

    draw a square with turtle module

---
+++ Python turtle graphic/draw_square.py
@@ -0,0 +1,12 @@
+# draw a square with turtle module
+
+import turtle
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.hideturtle()
