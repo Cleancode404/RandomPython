07c393046b2de4751ed45fa02d791b122457481c
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 16:22:21 -0400

    cont game function

--- Allien_invasion/game_functions.py
+++ Allien_invasion/game_functions.py
@@ -1,13 +1,15 @@
 import sys
 import pygame
 
-def check_events(ship):
+def check_keydown_events(ship):
     """respond to keypressses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT():
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
+            check_keydown_evnet(event, ship)
+            
             if event.key == pygame.K_Right:
                 #move the ship to the right
                 #ship.rect.centerx += 1
@@ -17,13 +19,16 @@ def check_events(ship):
                 ship.moving_left = True
             
         elif event.type == pygame.KEYUP:
+            check_keydown_evnet(event, ship)
+            
             if event.key == pygame.K_RIGHT:
                     ship.moving_right = False
 
             elif event.key == pygame.K_LEFT:
                     ship.moving_right = False
 
-
+def check_keyup_event(event, ship):
+    """respond to keypressses and mouse events"""
 
 def update_screen(ai_settings, screen, ship):
     """update images on the screen and flip to the new screen"""
