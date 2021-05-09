ae315748a74723706de1dfae55e4622c20244cd5
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 15:59:31 -0400

    modify game function

--- Allien_invasion/game_functions.py
+++ Allien_invasion/game_functions.py
@@ -1,11 +1,18 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     """respond to keypressses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT():
             sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_Right:
+                #move the ship to the right
+                ship.rect.centerx += 1
+                
+
 
 def update_screen(ai_settings, screen, ship):
     """update images on the screen and flip to the new screen"""
@@ -15,4 +22,3 @@ def update_screen(ai_settings, screen, ship):
 
     #make the most recent drawn screen visible
     pygame.display.flip()
-    
