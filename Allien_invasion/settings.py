c5ef4df2e61221d9817e4b92ba88a3d49037caa5
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 15:24:02 -0400

    modify settings

--- Allien_invasion/settings.py
+++ Allien_invasion/settings.py
@@ -4,8 +4,16 @@ class Settings():
     """
 
     def __init__(self):
+
         "Initialize the game's setting"
         #screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        #ship settings
+        self.ship_speed_factor = 1.5
+
+        #bullet settings
+        self.bullet_speed_facotr = 1
+        
