12bb9f98261a4ce808147184407aeb38c6bcc858
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 18:45:39 -0400

    create ship

--- Allien_invasion/ship.py
+++ Allien_invasion/ship.py
@@ -1,41 +1,39 @@
 import pygame
+ 
+class Ship:
+    """A class to manage the ship."""
+ 
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
-class Ship():
-    def __init__(self, ai_settings, screen):
-        """
-        initialize the ship and set its starting position
-        """
-        self.ai_settings = ai_settings
-        self.screen = screen
-
-        #load the ship image and get its rect
-        self.image = pygame.image.load('Images/ship.bmp')
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
 
-        #start each new ship at the bottom center of the screen
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
 
-        #store a decimal value for the ship's center
-        self.center = float(self.rect.centerx)
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
 
-        #movemnt flag
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        "update ship location base on the movement flag"
-        #update the ship's center value, not the rectangle
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += self.ai_settings.ship_soeed_factor
-
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            self.rect.centerx -= self.ai_settings.ship_soeed_factor
-        
-        #update rect object from self.center
-        self.rect.centerx = self.center 
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
-        "Draw the ship at its current location"
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
