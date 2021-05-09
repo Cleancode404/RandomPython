c16f30373c71221905e1746770acd98aef30723f
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 16:10:32 -0400

    cont game functions

--- Allien_invasion/game_functions.py
+++ Allien_invasion/game_functions.py
@@ -13,8 +13,14 @@ def check_events(ship):
                 #ship.rect.centerx += 1
                 ship.moving_right = True
             
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+            
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                    ship.moving_right = False
+
+            elif event.key == pygame.K_LEFT:
                     ship.moving_right = False
 
 
