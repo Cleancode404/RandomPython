50694ac0da6ff47aa97cc8a8227e675abc7d1e3c
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 18:45:50 -0400

    create settings

--- Allien_invasion/settings.py
+++ Allien_invasion/settings.py
@@ -1,23 +1,12 @@
-class Settings():
-    """
-    a class to store all settings for alien invasion
-    """
+class Settings:
+    """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-
-        "Initialize the game's setting"
-        #screen settings
+        """Initialize the game's settings."""
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        #ship settings
-        self.ship_speed_factor = 1.5
-
-        #bullet settings
-        self.bullet_speed_facotr = 1
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
-        self.bullet_allowed = 3
-
+        # Ship settings
+        self.ship_speed = 1.5
