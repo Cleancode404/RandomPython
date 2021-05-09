f4188db5e52e85af5ede15fc7ad3b226a48895dc
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 15:15:39 -0400

    creat setting class

--- Allien_invasion/settings.py
+++ Allien_invasion/settings.py
@@ -1,1 +1,11 @@
-class 
+class Settings():
+    """
+    a class to store all settings for alien invasion
+    """
+
+    def __init__(self):
+        "Initialize the game's setting"
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
