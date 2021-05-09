10056ed9a3a9cc74bf987517d421609cc01a2d67
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 18:45:29 -0400

    creat bullet object

--- Allien_invasion/bullet.py
+++ Allien_invasion/bullet.py
@@ -19,4 +19,16 @@ class Bullet(Sprite):
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
-        
+
+
+    def update(self):
+        """Move the bullet up the screen"""
+        #update the decimal position of the bullet
+        self.y -= self.speed_factor
+
+        #update the rect position
+        self.rect.py = self.y
+
+    def draw_bullet(self):
+        "draw bullet to the screen"
+        pygame.draw.rect(self.screen, self.color, self.rect)
