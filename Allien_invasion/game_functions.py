d701cdfbb9ef94c2576dd01c383d16241bd5e33b
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 18:26:53 -0400

    cont game functions

--- Allien_invasion/game_functions.py
+++ Allien_invasion/game_functions.py
@@ -25,8 +25,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 
     elif event.key == pygame.K_SPACE:
         #create a new bullet and add it to the bullets group
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
         """respond to keypressses """
@@ -44,7 +45,7 @@ def update_screen(ai_settings, screen, ship, bullets):
     #redraw for all bullets behind ship and aliens
     for bullet in bullets.sprites():
         bullet.draw_bullet()
-        
+
     ship.blitme()
 
     #make the most recent drawn screen visible
