abe1a169014c7fe1b24e764ff7954d9dcc71cf56
Heuristic <codemonkey2468@hotmail.com>
2021-05-09 16:00:14 -0400

    cont alien invasion

--- Allien_invasion/allien_invasion.py
+++ Allien_invasion/allien_invasion.py
@@ -25,7 +25,7 @@ def run_game():
     #start the main loop for the game
     while True:
 
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
 
         #watch for keyboard and mouse events.
