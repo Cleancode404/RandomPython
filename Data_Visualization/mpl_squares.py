955dbb6714a47be4415d43658b8e18df647e7af1
Heuristic <codemonkey2468@hotmail.com>
2021-06-20 11:40:07 -0400

    cont plot

--- Data_Visualization/mpl_squares.py
+++ Data_Visualization/mpl_squares.py
@@ -2,6 +2,13 @@ import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth = 3)
 
+#set chart title and label axes
+ax.set_title("Square numbers", fontsize = 26)
+ax.set_xlabel("Input", fontsize = 15)
+ax.set_ylabel("Output", fontsize = 15)
+
+#set size of tick labels
+ax.tick_params(axis = 'both', labelsize = 15)
 plt.show()
